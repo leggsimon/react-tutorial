607335b862c932295336fdbd83683099af03dd89
Simon Legg <leggsimon@gmail.com>
2016-02-01 20:44:31 +0000

    render component in div with id #app

--- app/main.jsx
+++ app/main.jsx
@@ -1,1 +1,7 @@
+var React = require('react/addons');
+
 console.log('Hello from the JSX!');
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+React.render(<GroceryItemList />, app)
