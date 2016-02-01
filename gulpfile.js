a622f2169bf50d654c72b5b34a4217d674ffd77e
Simon Legg <leggsimon@gmail.com>
2016-02-01 20:29:58 +0000

    added `gulp bundle` command

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
 
 gulp.task('live-server', function () {
   var server = new LiveServer('server/main.js');
@@ -12,4 +15,15 @@ gulp.task('serve', ['live-server'], function () {
     proxy:"http://localhost:3000",
     port: 9001
   });
+})
+
+gulp.task('bundle', function () {
+  return browserify({
+    entries: 'app/main.jsx',
+    debug: true
+  })
+  .transform(reactify)
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./.tmp'));
 })
